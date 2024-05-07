a30b6cfb42f6c94d6a019816937c6c7378a22d5c
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2024-05-07 10:36:24 +0300

    Initial commit

---
+++ src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './LoadMoreButton.module.css';
+
+const LoadMoreButton = ({handleClick}) => {
+  return (
+    <div>
+      <button className={css.loadMoreButton} type="button" onClick={handleClick}>Load more</button>
+    </div>
+  )
+}
+
+export default LoadMoreButton
